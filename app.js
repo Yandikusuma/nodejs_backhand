7b9345e54d30bcd9030867a13e34bd55a4e324b3
Yandikusuma <yandibaharselatan@gmail.com>
2018-03-13 16:13:36 +0700

    initial project

---
+++ app.js
@@ -0,0 +1,45 @@
+//---Anonymus function--
+
+//var waterair = function(air){
+//	console.log(air);
+//}
+
+/*function waterair(air){
+	console.log(air);
+}*/
+//waterair("hello world");
+
+//function manywater(watermain, value){
+//	watermain(value);
+//}
+//manywater(function(air){console.log(air);},"hello yandik")
+
+//---Global object n timers--
+
+//console.log(__filename);
+//console.log(__dirname);
+
+//function printstuff(){
+//	console.log("This was from setInterval");
+//}
+
+//setTimeout(printstuff,4000);
+
+//setInterval(printstuff,2000);
+
+
+//----- Callback function--
+
+/*console.log("User 1 made a pizaa");
+setTimeout(callback, 3000);
+
+console.log("User 2 made a pizaa");
+setTimeout(callback, 3000);
+
+console.log("User 3 made a pizaa");
+setTimeout(callback, 3000);
+
+function callback(){
+	console.log("queried database and delivered data in 5");
+}
+*/
---
+++ athletic.js
@@ -0,0 +1,8 @@
+function relay(){
+	console.log("This is relay function");
+}
+function shortjump(){
+	console.log("This is shortjump function");
+}
+
+module.exports.relay = relay
